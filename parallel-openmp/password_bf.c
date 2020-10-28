fc951937112f2447e1e23a4f9b9140256f71250f
Douglas Toledo da Silva <douglas22toledo@gmail.com>
2020-10-27 21:06:12 -0300

    parellized

--- parallel-openmp/password_bf.c
+++ parallel-openmp/password_bf.c
@@ -6,7 +6,7 @@
 
 // MD5_DIGEST_LENGTH = 16
 
-#define MAX 6 // max size of password
+#define MAX 7 // max size of password
 
 char letters[] = "ABCDEFGHIJKLMNOPQRS1234567890"; // 36 possible symbols
 
@@ -60,6 +60,7 @@ void iterate(byte *hash1, byte *hash2, char *str, int idx, int *ok)
 
 			printf("found: %s\n", str);
 			print_digest(hash2);
+			#pragma omp critical
 			*ok = 1;
 		}
 		// Recursive call
@@ -86,11 +87,9 @@ int main(int argc, char **argv)
 {
 	Ticks[0] = clock();
 
-	char str[MAX + 1];
 	int ok = 0, r;
 	char hash1_str[2 * MD5_DIGEST_LENGTH + 1];
 	byte hash1[MD5_DIGEST_LENGTH]; // password hash
-	byte hash2[MD5_DIGEST_LENGTH]; // string hashes
 
 	// Input:
 	r = scanf("%s", hash1_str);
@@ -105,32 +104,38 @@ int main(int argc, char **argv)
 	// Convert hexadecimal string to hash byte.
 	strHex_to_byte(hash1_str, hash1);
 
-	memset(hash2, 0, MD5_DIGEST_LENGTH);
-
 	print_digest(hash1);
-
-	// Generate all possible passwords of different sizes.
-	memset(str, 0, MAX + 1);
 
 	int c;
 
-	for (c = 0; c < strlen(letters); ++c)
+	#pragma omp parallel shared(ok, hash1)
 	{
-		if (ok == 0) {
-			str[0] = letters[c];
-			// check if password matches
-			MD5((byte *)str, 1, hash2);
-			if (strncmp((char *)hash1, (char *)hash2, MD5_DIGEST_LENGTH) == 0)
-			{
-				str[1] = 0;
+		byte hash2[MD5_DIGEST_LENGTH]; // string hashes
+		memset(hash2, 0, MD5_DIGEST_LENGTH);
 
-				printf("found: %s\n", str);
-				print_digest(hash2);
-				ok = 1;
+		char str[MAX + 1];
+		memset(str, 0, MAX + 1);
+
+		#pragma omp for
+		for (c = 0; c < strlen(letters); ++c)
+		{
+			if (ok == 0) {
+				str[0] = letters[c];
+				// check if password matches
+				MD5((byte *)str, 1, hash2);
+				if (strncmp((char *)hash1, (char *)hash2, MD5_DIGEST_LENGTH) == 0)
+				{
+					str[1] = 0;
+
+					printf("found: %s\n", str);
+					print_digest(hash2);
+					#pragma omp critical
+						ok = 1;
+				}
 			}
+			// Recursive call
+			iterate(hash1, hash2, str, 1, &ok);
 		}
-		// Recursive call
-		iterate(hash1, hash2, str, 1, &ok);
 	}
 
 	print_time(1);
